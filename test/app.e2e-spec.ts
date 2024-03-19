fd8378011bd3e0adc64c690c7bd30241ed86378f
gwonyeong <qwe980121@naver.com>
2024-03-19 21:58:02 +0900

    feat: e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { UserPoint } from '@/point/point.model';
+import { UserPoint, TransactionType, PointHistory } from '@/point/point.model';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -38,6 +38,45 @@ describe('AppController (e2e)', () => {
         point: 100,
         updateMillis: expect.any(Number),
       });
+
+      const chargeData2: {
+        status: number;
+        body: UserPoint;
+      } = await request(app.getHttpServer())
+        .patch('/point/1/charge')
+        .send({ amount: 200 });
+
+      expect(chargeData2.status).toBe(200);
+      expect(chargeData2.body).toEqual({
+        id: 1,
+        point: 200,
+        updateMillis: expect.any(Number),
+      });
+
+      const historyData: {
+        status: number;
+        body: PointHistory[];
+      } = await request(app.getHttpServer()).get('/point/1/histories');
+
+      expect(historyData.status).toBe(200);
+      expect(historyData.body).toHaveLength(2);
+
+      expect(historyData.body).toEqual([
+        {
+          id: 1,
+          userId: 1,
+          amount: 100,
+          type: TransactionType.CHARGE,
+          timeMillis: expect.any(Number),
+        },
+        {
+          id: 2,
+          userId: 1,
+          amount: 200,
+          type: TransactionType.CHARGE,
+          timeMillis: expect.any(Number),
+        },
+      ]);
     });
   });
 });
